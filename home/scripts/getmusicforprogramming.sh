ca9f545793233baedc2e49af6dc5ce62b7243dc2
host <cuzrawr@example.com>
2020-06-27 13:18:22 +0300

    unnecessary

--- home/scripts/getmusicforprogramming.sh
+++ home/scripts/getmusicforprogramming.sh
@@ -2,4 +2,4 @@
 # getmusicforprogramming.sh - get my favorite music. With progress bar!!!
 #
 
-curl -sXGET 'https://musicforprogramming.net/rss.php'  | grep "guid"  | sed -E 's/<guid>|<\/guid>//g' |  xargs -n1 curl -L -O -C - --progress-bar -w '%{url_effective}\n'
+curl 'https://musicforprogramming.net/rss.php'  | grep "guid"  | sed -E 's/<guid>|<\/guid>//g' |  xargs -n1 curl -L -O -C - --progress-bar -w '%{url_effective}\n'
