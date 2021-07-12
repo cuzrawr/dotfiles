b596ab9245c77fff50631b647810002a6b2e2817
cuzrawr <46599814+cuzrawr@users.noreply.github.com>
2021-07-12 18:46:37 +0300

    fix nm

--- etc/NetworkManager/dispatcher.d/pre-up.d/11-qdisc-pie.sh
+++ etc/NetworkManager/dispatcher.d/pre-up.d/11-qdisc-pie.sh
@@ -2,9 +2,9 @@
 
 # https://viliampucik.wordpress.com/2016/09/30/networkmanager-disable-sending-hostname-to-dhcp-server/
 
-IFS=$'\n'
-for i in $(grep ^id= /etc/NetworkManager/system-connections/*.nmconnection | cut -d= -f2); do \
-    nmcli connection modify "$i" tc.qdiscs 'root noqueue'; \
-done
-echo "11111111111111111111111111111111111111111111111111112"
-logger "loaded script test qdisc noqueue"
+#IFS=$'\n'
+#for i in $(grep ^id= /etc/NetworkManager/system-connections/*.nmconnection | cut -d= -f2); do \
+#    nmcli connection modify "$i" tc.qdiscs 'root noqueue'; \
+#done
+#echo "11111111111111111111111111111111111111111111111111112"
+#logger "loaded script test qdisc noqueue"
