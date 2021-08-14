bb225fb2c4eea11e62d0927a090005747eac899a
cuzrawr <46599814+cuzrawr@users.noreply.github.com>
2021-08-15 01:27:20 +0300

    test f2fs

--- tweaks.sh
+++ tweaks.sh
@@ -9,3 +9,9 @@ tune2fs -l
 #
 # check
 chattr +i ~/.config/xfce4/xfce4-screenshooter
+
+#
+# f2fs hot list
+# -E "db,rng,pyi,h,save,dic,lck,lock,bin,cc,c,cpp,h,a,o,asm,deps,pl,rb,am,key,dat,conf,cnf,rc,xrdb,gpg,mime,yml,yaml,ssh,dirs,tmp,so"
+# as an example: 
+# mkfs.f2fs -f -E "db,rng,pyi,h,save,dic,lck,lock,bin,cc,c,cpp,h,a,o,asm,deps,pl,rb,am,key,dat,conf,cnf,rc,xrdb,gpg,mime,yml,yaml,ssh,dirs,tmp,so" -l LABEL /dev/sdXX
