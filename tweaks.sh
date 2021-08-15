9d5c1d5f02edb912931d9beab8bb63d3028a6db8
cuzrawr <46599814+cuzrawr@users.noreply.github.com>
2021-08-15 04:20:54 +0300

    intel hd are trash, chromium

--- tweaks.sh
+++ tweaks.sh
@@ -15,3 +15,7 @@ chattr +i ~/.config/xfce4/xfce4-screenshooter
 # -E "db,rng,pyi,h,save,dic,lck,lock,bin,cc,c,cpp,h,a,o,asm,deps,pl,rb,am,key,dat,conf,cnf,rc,xrdb,gpg,mime,yml,yaml,ssh,dirs,tmp,so"
 # as an example: 
 # mkfs.f2fs -f -E "db,rng,pyi,h,save,dic,lck,lock,bin,cc,c,cpp,h,a,o,asm,deps,pl,rb,am,key,dat,conf,cnf,rc,xrdb,gpg,mime,yml,yaml,ssh,dirs,tmp,so" -l LABEL /dev/sdXX
+
+
+# my rsync variant to clone OS
+# rsync -aHAXvxr --progress /from/ /to/
