2506db3873bd8a952cfcc6d5a7036ec3c94aa020
cuzrawr <46599814+cuzrawr@users.noreply.github.com>
2021-09-23 14:50:28 +0300

    custom aliases upd

--- etc/profile.d/00custom.sh
+++ etc/profile.d/00custom.sh
@@ -37,6 +37,9 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+alias e="nano"
+
+
 #
 alias less='less --use-color '
 
@@ -47,4 +50,41 @@ alias reboot='sync; echo 3 > /proc/sys/vm/drop_caches; sync; reboot'
 if [ "$SHELL" = "/bin/bash" ]; then alias history="cat ~/.bash_history"; fi
 
 #. "/usr/share/LS_COLORS/dircolors.sh"
-
+##
+##
+##
+# Archives
+function extract {
+  if [ -z "$1" ]; then
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+  else
+    if [ -f $1 ]; then
+      case $1 in
+        *.tar.bz2)   tar xvjf $1    ;;
+        *.tar.gz)    tar xvzf $1    ;;
+        *.tar.xz)    tar xvJf $1    ;;
+        *.lzma)      unlzma $1      ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar x -ad $1 ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xvf $1     ;;
+        *.tbz2)      tar xvjf $1    ;;
+        *.tgz)       tar xvzf $1    ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *.xz)        unxz $1        ;;
+        *.exe)       cabextract $1  ;;
+        *)           echo "extract: '$1' - unknown archive method" ;;
+      esac
+    else
+      echo "$1 - file does not exist"
+    fi
+  fi
+}
+alias extr='extract '
+function extract_and_remove {
+  extract $1
+  rm -f $1
+}
+alias extrr='extract_and_remove '
